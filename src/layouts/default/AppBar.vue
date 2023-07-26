ab167afe8ac5094b38a9bffd0a14c60ce9b5cd3b
TOVTC <veronica.tc.to@outlook.com>
2023-07-26 12:30:48 -0400

    placeholder updates to AppBar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,22 @@
+<script setup>
+  // import { ref, VueElement } from 'vue';
+
+  function placeholder(term) {
+    console.log(term);
+  }
+</script>
+
 <template>
   <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
-
-      Base Preset
+    <v-app-bar-title @click="placeholder('homepage')">
+      <v-icon>
+        <v-img src="@/assets/logo.png"/>
+      </v-icon>
+      Movie Lookup
     </v-app-bar-title>
+    <v-btn text @click="placeholder('trending')">Trending</v-btn>
+    <v-btn text @click="placeholder('popular')">Popular</v-btn>
+    <v-btn text @click="placeholder('top rated')">Top Rated</v-btn>
+    <v-btn text @click="placeholder('now playing')">Now Playing</v-btn>
   </v-app-bar>
 </template>
-
-<script setup>
-  //
-</script>
