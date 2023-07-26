1e3a6ffd5ddf8d5ba424ef1f19378fd9951bea26
TOVTC <veronica.tc.to@outlook.com>
2023-07-26 16:47:38 -0400

    added homepage layout, refactored default app structure

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import AppBar from '@/components/AppBar.vue'
+  import AppView from '@/layouts/MainView.vue'
+  import AppFooter from '@/components/AppFooter.vue'
+</script>
+
+<template>
+  <v-app>
+    <app-bar />
+
+    <app-view />
+
+    <app-footer />
+  </v-app>
+</template>
