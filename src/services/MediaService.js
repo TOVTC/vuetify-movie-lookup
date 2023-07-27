9b4c6dadb01cbb48d30facc00236dcdd8dd44968
TOVTC <veronica.tc.to@outlook.com>
2023-07-27 16:46:12 -0400

    added media service functions

--- src/services/MediaService.js
+++ src/services/MediaService.js
@@ -1,15 +1,78 @@
-// import axios from 'axios';
-// import { apikey } from '../../apikey';
+import axios from 'axios'
+import { apikey } from '../../apikey';
 
-// const apiClient = axios.create({
-//     baseURL: 'https://api.themoviedb.org',
-//     withCredentials: false,
-//     headers: {
-//         Accept: 'application/json',
-//         'Content-Type': 'application/json'
-//     }
-// });
+const apiClient = axios.create({
+    baseURL: 'https://api.themoviedb.org',
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
 
-// export default {
-
-// }
+export default {
+    async getResults(name) {
+        try {
+            return await apiClient.get(`/3/search/movie?api_key=${apikey}&language=en-US&query=${name}&page=1&include_adult=false`);
+        }
+        catch (err) {
+            console.log(err)
+        }
+    },
+    async getDetails(id) {
+        try {
+            return await apiClient.get(`/3/movie/${id}?api_key=${apikey}`);
+        }
+        catch (err) {
+            console.log(err)
+        }
+    },
+    async getTrending() {
+        try {
+            return await apiClient.get(`/3/trending/movie/day?api_key=${apikey}&language=en-US&page=1`);
+        }
+        catch (err) {
+            console.log(err)
+        }
+    },
+    async getPopular() {
+        try {
+            return await apiClient.get(`/3/movie/popular?api_key=${apikey}&language=en-US&page=1`);
+        }
+        catch (err) {
+            console.log(err)
+        }
+    },
+    async getTopRated() {
+        try {
+            return await apiClient.get(`/3/movie/top_rated?api_key=${apikey}&language=en-US&page=1`);
+        }
+        catch (err) {
+            console.log(err)
+        }
+    },
+    async getNowPlaying() {
+        try {
+            return await apiClient.get(`/3/movie/now_playing?api_key=${apikey}&language=en-US&page=1`);
+        }
+        catch (err) {
+            console.log(err)
+        }
+    },
+    async getRecommended(id) {
+        try {
+            return await apiClient.get(`/3/movie/${id}/recommendations?api_key=${apikey}&language=en-US&page=1`);
+        }
+        catch (err) {
+            console.log(err)
+        }
+    },
+    async getSimilar(id) {
+        try {
+            return await apiClient.get(`/3/movie/${id}/similar?api_key=${apikey}&language=en-US&page=1`);
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+}
